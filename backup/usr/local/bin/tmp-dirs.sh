0d5f026faa8153243b3b80708b2d1ea49a8e4c80
Kiarash Korki <kiarash.96@gmail.com>
2016-09-12 12:52:56 +0430

    Spotify download support

--- backup/usr/local/bin/tmp-dirs.sh
+++ backup/usr/local/bin/tmp-dirs.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+mkdir -p /mnt/archive/
+mkdir -p /mnt/data/
+mkdir -p /tmp/kiarash/
+
 # link spotify cache dirs to /tmp
-mkdir -p /tmp/kiarash/.cache/spotify
-ln -s /tmp/kiarash/.cache/spotify /home/kiarash/.cache/spotify
+mkdir -p /tmp/kiarash/.cache/spotify/Storage
+mkdir -p /mnt/archive/cache/
+ln -s /tmp/kiarash/.cache/spotify/ /mnt/archive/cache/
+ln -s /mnt/archive/cache/spotify/ /home/kiarash/.cache/
 
 # create pacman cache dir
 mkdir -p /tmp/pacman/pkg/
+mkdir -p /mnt/archive/
+ln -s /tmp/pacman/pkg/ /mnt/archive/
 
 # nvim tmp dirs
 mkdir -p /tmp/kiarash/.nvim/backup/
