502a60fb612bef4f5391deada566a31c03c5049b
Kiarash Korki <kiarash.96@gmail.com>
2017-04-05 22:04:14 +0430

    Display an status bar in zsh

--- storage/home/kiarash/.config/zsh/prompt.zsh
+++ storage/home/kiarash/.config/zsh/prompt.zsh
@@ -1,6 +1,33 @@
-PROMPT_SYMBOL=''
+#
+# show a customized prompt symbol + an status bar
+# TODO: a less hacky solution for displaying status bar? :\
+#
 
-PROMPT="%F{magenta}[%~]%f" # current working directory
-PROMPT+=" %(?.%F{green}.%F{9})❯%f " # symbol in green/red
+PROMPT="%(?.%F{green}.%F{9})❯%f " # symbol in green/red
 
-# TODO: git
+precmd() { 
+    print -Pn "\033[s\033[1;1H%E%F{magenta}[%~]%f\033[u"
+}
+
+
+__status-init() {
+    print -Pn "\n"
+}
+__status-ctrl-l() {
+    zle && zle kill-buffer && zle -R
+    clear
+    precmd
+    print -Pn "\n$PROMPT"
+}
+__status-clear() {
+    clear
+    precmd
+    print -Pn "$PROMPT"
+}
+
+zle -N __status-ctrl-l
+bindkey "^L" __status-ctrl-l
+alias clear=__status-clear
+__status-init
+
+# TODO: jobs + git info
