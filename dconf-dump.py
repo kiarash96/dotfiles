30ad998e61cc2f4a346d61f6bb092262e8980dcb
Kiarash Korki <kiarash.96@gmail.com>
2022-08-18 14:07:07 +0430

    Improve backup scripts

--- dconf-dump.py
+++ dconf-dump.py
@@ -1,4 +1,9 @@
-#!/bin/env python3
+#!/usr/bin/env python3
+
+#
+# This script reads a list of keys in yaml format and prints their value in
+# dconf database
+#
 
 import sys
 import subprocess
@@ -10,7 +15,8 @@ def backup(path, obj):
     if type(obj) == list:
         print('\n' + '[' + path[1:] + ']' + '\n', end='')
         for key in obj:
-            result = subprocess.run(['dconf', 'read', path + '/' + key], capture_output=True)
+            result = subprocess.run(['dconf', 'read', path + '/' + key],
+                    capture_output=True)
             if result.stdout:
                 print(key + '=' + result.stdout.decode(), end='')
             else:
@@ -21,10 +27,10 @@ def backup(path, obj):
         backup(path + '/' + str(key).replace('.', '/'), value)
 
 def main():
-    with open('./watchlist-dconf.yml') as f:
-        watchlist = yaml.safe_load(f)
+    watchlist = yaml.safe_load(sys.stdin)
     backup('', watchlist)
 
 if __name__ == "__main__":
     main()
 
+# vim: ts=4 sw=4 sts=4 et
